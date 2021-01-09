7364f0090c5d5caa04525b2ef7e007ee1800628a
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-01-09 13:21:44 +0530

    Added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) VALUES ('South Indian');
+INSERT INTO category (description) VALUES ('Punjabi');
+INSERT INTO category (description) VALUES ('Bihari');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Milligram');
