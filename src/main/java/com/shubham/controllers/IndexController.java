28438febdc9c549eca83ce75fb0d0c73a9b9c210
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-01-12 12:52:25 +0530

    Added Lombok in the project

--- src/main/java/com/shubham/controllers/IndexController.java
+++ src/main/java/com/shubham/controllers/IndexController.java
@@ -5,12 +5,14 @@ import com.shubham.domain.UnitOfMeasure;
 import com.shubham.repositories.CategoryRepository;
 import com.shubham.repositories.UnitOfMeasureRepository;
 import com.shubham.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -22,6 +24,8 @@ public class IndexController {
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(Model model){
+
+        log.debug("Getting Index Page");
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
--- src/main/java/com/shubham/services/RecipeServiceImpl.java
+++ src/main/java/com/shubham/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.shubham.services;
 
 import com.shubham.domain.Recipe;
 import com.shubham.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService{
 
@@ -19,10 +21,10 @@ public class RecipeServiceImpl implements RecipeService{
     @Override
     public Set<Recipe> getRecipes() {
 
+        log.debug("I am in the RecipeServiceImpl");
+
         Set<Recipe> recipeSet = new HashSet<>();
-
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
-
         return recipeSet;
     }
 }
