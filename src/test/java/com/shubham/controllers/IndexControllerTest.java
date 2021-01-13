fac3b74858eadd0b31a00e48642eb3e75b1a38fc
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-01-13 13:59:06 +0530

    Tested Spring MockMVC

--- src/test/java/com/shubham/controllers/IndexControllerTest.java
+++ src/test/java/com/shubham/controllers/IndexControllerTest.java
@@ -6,19 +6,21 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anySet;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IndexControllerTest {
@@ -35,6 +37,13 @@ public class IndexControllerTest {
     @Before
     public void setUp() throws Exception {
         controller = new IndexController(recipeService);
+    }
+
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk()).andExpect(view().name("index"));
     }
 
     @Test
---
+++ src/test/java/com/shubham/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,15 @@
+package com.shubham.repositories;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UnitOfMeasureRepositoryIT {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByDescription() {
+    }
+}
