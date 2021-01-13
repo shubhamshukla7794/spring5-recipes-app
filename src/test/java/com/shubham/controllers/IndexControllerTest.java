6835959cc4b6eeb35c6274c7d05675c837e59833
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-01-13 09:12:06 +0530

    Added Mockito Argument Capture in test case for IndexController

--- src/test/java/com/shubham/controllers/IndexControllerTest.java
+++ src/test/java/com/shubham/controllers/IndexControllerTest.java
@@ -1,19 +1,24 @@
 package com.shubham.controllers;
 
+import com.shubham.domain.Recipe;
 import com.shubham.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class IndexControllerTest {
@@ -34,9 +39,26 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
+
+        //Given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        //When
         String viewName = controller.getIndexPage(model);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //Then
         assertEquals("index",viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2,setInController.size());
     }
 }
