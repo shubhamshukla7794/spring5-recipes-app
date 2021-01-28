6cdf8526a9830f03baad99934ad8449c50be3466
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-01-28 09:50:46 +0530

    Added Header and Footer Layout

--- src/main/java/com/shubham/controllers/IndexController.java
+++ src/main/java/com/shubham/controllers/IndexController.java
@@ -1,16 +1,10 @@
 package com.shubham.controllers;
 
-import com.shubham.domain.Category;
-import com.shubham.domain.UnitOfMeasure;
-import com.shubham.repositories.CategoryRepository;
-import com.shubham.repositories.UnitOfMeasureRepository;
 import com.shubham.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -22,7 +16,7 @@ public class IndexController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping({"","/","/index"})
+    @RequestMapping({"","/","/index","/home"})
     public String getIndexPage(Model model){
 
         log.debug("Getting Index Page");
