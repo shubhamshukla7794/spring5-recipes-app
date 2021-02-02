3cf99cfe56bbfb89aba222d332a7591f33231f40
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-02-02 11:24:30 +0530

    Added Spring MVC Exception Handler

--- src/main/java/com/shubham/controllers/RecipeController.java
+++ src/main/java/com/shubham/controllers/RecipeController.java
@@ -1,11 +1,16 @@
 package com.shubham.controllers;
 
 import com.shubham.commands.RecipeCommand;
+import com.shubham.exceptions.NotFoundException;
 import com.shubham.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -53,6 +58,16 @@ public class RecipeController {
     public String deleteById(@PathVariable String id){
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        return modelAndView;
     }
 
 }
