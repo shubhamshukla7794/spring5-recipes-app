beaad4dab6c50697a30d68a550dd10ea7cc5887a
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-02-02 14:00:23 +0530

    Showing Error Data on 404 Error Page

--- src/main/java/com/shubham/services/RecipeServiceImpl.java
+++ src/main/java/com/shubham/services/RecipeServiceImpl.java
@@ -45,8 +45,7 @@ public class RecipeServiceImpl implements RecipeService{
         Optional<Recipe> recipeOptional = recipeRepository.findById(id);
 
         if (!recipeOptional.isPresent()){
-//            throw new RuntimeException("Recipe Not Found!!!");
-            throw new NotFoundException("Recipe Not Found!!!");
+            throw new NotFoundException("Recipe Not Found. For the ID value " + id.toString());
         }
 
         return recipeOptional.get();
